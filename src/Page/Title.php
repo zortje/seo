2d968320bb63e42a6115055d22b62e127a2e2404
René Tønner Stokholm <zortje@gmail.com>
2016-06-04 19:42:30 +0200

    CS

--- src/Page/Title.php
+++ src/Page/Title.php
@@ -7,47 +7,51 @@ namespace Zortje\SEO\Page;
  *
  * @package Zortje\SEO\Page
  */
-class Title {
+class Title
+{
 
-	/**
-	 * @var string
-	 */
-	private $title;
+    /**
+     * @var string
+     */
+    private $title;
 
-	/**
-	 * @param string $title Page title
-	 */
-	public function setTitle($title) {
-		$this->title = $title;
-	}
+    /**
+     * @param string $title Page title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * @return string Page title
-	 */
-	public function getTitle() {
-		return $this->title;
-	}
+    /**
+     * @return string Page title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * Analyze title for issues
-	 *
-	 * @return array Issues; empty if optimized
-	 */
-	public function analyzeForIssues() {
-		$issues = [];
+    /**
+     * Analyze title for issues
+     *
+     * @return array Issues; empty if optimized
+     */
+    public function analyzeForIssues()
+    {
+        $issues = [];
 
-		/**
-		 * Length
-		 */
-		if (strlen($this->title) === 0) {
-			$issues[] = 'Title is not set or empty';
-		}
+        /**
+         * Length
+         */
+        if (strlen($this->title) === 0) {
+            $issues[] = 'Title is not set or empty';
+        }
 
-		if (strlen($this->title) > 55) {
-			$issues[] = 'Title is longer than 55 characters';
-		}
+        if (strlen($this->title) > 55) {
+            $issues[] = 'Title is longer than 55 characters';
+        }
 
-		return $issues;
-	}
+        return $issues;
+    }
 
 }
